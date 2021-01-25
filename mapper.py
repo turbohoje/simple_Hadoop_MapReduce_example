0c39591591e6eca65d133899e35bd33b63dcdcdf
turbohoje <justin@rocketscience.cc>
2021-01-25 00:46:09 +0000

    change to stop words

--- mapper.py
+++ mapper.py
@@ -13,7 +13,8 @@ for line in sys.stdin:
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
-    stopwords = set(['the', 'and'])
+    stopwords = set(['the', 'and', 'glamis'])
+
 
     for word in words:
         if word not in stopwords:
